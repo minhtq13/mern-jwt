8a9aa123230c0d16129094833e3e46b4d8f1c86a
minhtq13 <taminh596@gmail.com>
2023-02-21 22:51:07 +0700

    update 21/02 34:09

---
+++ back-end/routes/user.js
@@ -0,0 +1,13 @@
+const middlewareControlller = require("../controllers/middlewareController");
+const userController = require("../controllers/userController");
+
+const router = require("express").Router();
+
+// GET ALL USERS
+
+router.get("/", middlewareControlller.verifyToken, userController.getAllUsers);
+
+// DELETE USER
+
+router.delete("/:id", userController.deleteUser);
+module.exports = router;
